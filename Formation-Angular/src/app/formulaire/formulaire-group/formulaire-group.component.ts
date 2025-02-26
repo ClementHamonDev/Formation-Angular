8344fe4de7f261f87d525140dd5ff92dce66a72e
ClementHamonDev <clement.hamon35@gmail.com>
2025-02-26 17:32:38 +0100

    correction exercice form group

--- Formation-Angular/src/app/formulaire/formulaire-group/formulaire-group.component.ts
+++ Formation-Angular/src/app/formulaire/formulaire-group/formulaire-group.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-group',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './formulaire-group.component.html',
   styleUrl: './formulaire-group.component.css'
 })
@@ -12,16 +12,20 @@ export class FormulaireGroupComponent {
 
   formulaire: FormGroup;
 
-  constructor(private fb: FormBuilder){
+
+  constructor(private fb: FormBuilder) {
     this.formulaire = this.fb.group(
       {
         nom: ['', [Validators.required, Validators.minLength(3)]],
         email: ['', [Validators.required, Validators.email]],
+        message: ['', [Validators.required, Validators.minLength(10)]],
+        langue: ["", [Validators.required]]
       }
     )
   }
 
   onSubmit() {
+    alert("Formulaire soumis");
     console.log('Formulaire soumis! ', this.formulaire.value);
   }
 

c9b70914ff52769c90e719122453bc4c199da479
ClementHamonDev <clement.hamon35@gmail.com>
2025-03-03 10:19:35 +0100

    directives

---
+++ Formation-Angular/src/app/directives/highlight.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private el: ElementRef) {}
+
+  @Input() colorInput = '';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.colorInput || 'red');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
